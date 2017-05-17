ace3c864fd8602fbc96acae99e6acfb24848ed15
ensirius <ensirius@gmail.com>
2017-05-17 17:48:42 +0300

    Добавлено сокращение слов

--- exporter/src/main/java/ru/ensemplix/shop/ShopItemNameConverter.java
+++ exporter/src/main/java/ru/ensemplix/shop/ShopItemNameConverter.java
@@ -2,27 +2,52 @@ package ru.ensemplix.shop;
 
 public class ShopItemNameConverter {
 
+    // Максимальное количество символов на одной строке таблички.
+    private static final int MAX_SIGN_LINE_TEXT_WIDTH = 16;
+
     /**
      * Конвертирует имя предмета для использования на табличке.
+     *
+     * 1. Заменяет все символы, кроме букв, на пробелы.
+     * 2. Убирает лишние пробелы между словами.
+     * 3. Вместо пробелов ставит нижнее подчеркивание.
+     * 4. Делает все буквы верхним регистром.
+     * 5. Сокращает слова, чтобы влезть в ограничение строки таблички.
      *
      * Примеры:
      *  - "Белая шерсть" => "БЕЛАЯ_ШЕРСТЬ"
      *  - "Термальная центрифуга" => "ТЕРМАЛЬ_ЦЕНТРИФУ"
+     *  - "Изолированный высоковольтный провод" => "ИЗОЛ_ВЫСОК_ПРОВО"
      *
      * @param name Имя предмета, которое мы конвертируем.
      * @return Преобразованное имя предмета.
      */
     public static String convert(String name) {
-        // Заменяет все символы, кроме букв, на пробелы.
-        name = name.replaceAll("[^a-zA-Zа-яА-Я0-9]", " ");
-        // Убирает лишние пробелы между словами.
-        name = name.replaceAll("\\s+"," ");
-        // Вместо пробелов ставит нижнее подчеркивание.
-        name = name.replaceAll(" ", "_");
-        // Делает все буквы верхним регистром.
-        name = name.toUpperCase();
-        // Сокращает слова, чтобы влезть в ограничение 16 символов.
-        // TODO: Добавить сокрашение слов.
+        name = name
+                .replaceAll("[^a-zA-Zа-яА-Я0-9]", " ")
+                .replaceAll("\\s+"," ")
+                .replaceAll(" ", "_")
+                .toUpperCase();
+
+        // Убирает по букве у самого длинного слова в процентном соотношении.
+        while(name.length() > MAX_SIGN_LINE_TEXT_WIDTH) {
+            String[] lines = name.split("_");
+            String maxLine = lines[0];
+            float maxPercent = 0;
+
+            for(String line : lines) {
+                float percent = line.length() / 100F;
+
+                if(percent > maxPercent) {
+                    maxLine = line;
+                    maxPercent = percent;
+                }
+            }
+
+            name = name.replaceAll(maxLine, maxLine.substring(0, maxLine.length() - 1));
+        }
+
+
         return name;
     }
 
--- exporter/src/test/java/ru/ensemplix/shop/ShopItemNameConverterTest.java
+++ exporter/src/test/java/ru/ensemplix/shop/ShopItemNameConverterTest.java
@@ -3,27 +3,33 @@ package ru.ensemplix.shop;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static ru.ensemplix.shop.ShopItemNameConverter.*;
 
 public class ShopItemNameConverterTest {
 
     @Test
     public void testConvertWool() {
-        assertEquals("БЕЛАЯ_ШЕРСТЬ", ShopItemNameConverter.convert("Белая шерсть"));
+        assertEquals("БЕЛАЯ_ШЕРСТЬ", convert("Белая шерсть"));
     }
 
     @Test
     public void testConvertWoolWithSymbol() {
-        assertEquals("БЕЛАЯ_ШЕРСТЬ", ShopItemNameConverter.convert("Белая - шерсть"));
+        assertEquals("БЕЛАЯ_ШЕРСТЬ", convert("Белая - шерсть"));
     }
 
     @Test
     public void testConvertWoolWithWhitespaces() {
-        assertEquals("БЕЛАЯ_ШЕРСТЬ", ShopItemNameConverter.convert("Белая   шерсть"));
+        assertEquals("БЕЛАЯ_ШЕРСТЬ", convert("Белая   шерсть"));
     }
 
     @Test
     public void testConvertCentrifuge() {
-        assertEquals("ТЕРМАЛЬ_ЦЕНТРИФУ", ShopItemNameConverter.convert("Термальная центрифуга"));
+        assertEquals("ТЕРМАЛЬ_ЦЕНТРИФУ", convert("Термальная центрифуга"));
+    }
+
+    @Test
+    public void testConvertWire() {
+        assertEquals("ИЗОЛ_ВЫСОК_ПРОВО", convert("Изолированный высоковольтный провод"));
     }
 
 }
