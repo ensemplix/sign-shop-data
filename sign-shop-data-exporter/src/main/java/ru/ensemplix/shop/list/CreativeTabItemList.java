b1d954aee96a476a318fe3d16bb968733c9e877d
ensirius <ensirius@gmail.com>
2017-05-18 20:49:44 +0300

    Не экспортируем лишние предметы

--- sign-shop-data-exporter/src/main/java/ru/ensemplix/shop/list/CreativeTabItemList.java
+++ sign-shop-data-exporter/src/main/java/ru/ensemplix/shop/list/CreativeTabItemList.java
@@ -1,8 +1,6 @@
 package ru.ensemplix.shop.list;
 
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
@@ -19,28 +17,6 @@ public class CreativeTabItemList implements ItemList {
 
         for(CreativeTabs creativeTab : CreativeTabs.creativeTabArray) {
             creativeTab.displayAllReleventItems(items);
-        }
-
-        for(Object obj : Item.itemRegistry) {
-            Item item = (Item) obj;
-
-            for(CreativeTabs creativeTab : item.getCreativeTabs()) {
-                item.getSubItems(item, creativeTab, items);
-            }
-        }
-
-        for(Object obj : Block.blockRegistry) {
-            Block block = (Block) obj;
-            Item item = Item.getItemFromBlock(block);
-
-            // Не все блоки имеют предмет.
-            if(item == null) {
-                continue;
-            }
-
-            for(CreativeTabs creativeTab : item.getCreativeTabs()) {
-                block.getSubBlocks(item, creativeTab, items);
-            }
         }
 
         return items;
